994e608398f91ff1310db4ce21ce3078995d0da2
Cho Young Jun <whdudwns7100@naver.com>
2022-06-09 22:40:26 +0900

    로프 ing

---
+++ 로프.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+	int rope_count;
+	
+	cin >> rope_count;  
+	
+	int rope_weight[n];
+	int rope_weight_sum = 0; 
+	int result;
+	
+	for (int i=0;i<n;i++)
+	{
+		cin >> rope_weight[i];
+		rope_weight_sum += rope_weight[i];
+	}
+	
+	
+	
+}
